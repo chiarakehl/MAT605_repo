b18835f9be0b1707975705432d1e2b881d3dcb3f
chiarakehl <chiarajil.kehl@uzh.ch>
2025-03-10 17:57:58 +0100

    solved the exercises up to ex.4

--- hw2_CK.hs
+++ hw2_CK.hs
@@ -37,5 +37,39 @@ zipWith' f as bs = [f a b | (a, b) <- zip' as bs]
 
 -- (iii) A function scalarProd :: (Num a) => [a] -> [a] -> a that returns the scalar product of two lists of numbers.
 scalarProd :: (Num a) => [a] -> [a] -> a
-scalarProd [] [] = 0
-scalarProd as bs = [zipWith' (*) as bs | (a, b)]
+scalarProd [a] [] = 0
+scalarProd [a] [b] = a * b
+scalarProd as bs = head (zipWith' (*) as bs) + scalarProd (tail as) (tail bs)
+
+{-
+--------------------------------------------------------------------------------------------------------------------
+Exercise 4
+--------------------------------------------------------------------------------------------------------------------
+-}
+
+-- Implement flatten' :: [[a]] -> [a], that flattens a list of lists
+flatten' :: [[a]] -> [a]
+flatten' [[]] = []
+flatten' xss = [x | xs <- xss, x <- xs]
+
+{-
+--------------------------------------------------------------------------------------------------------------------
+Exercise 5
+--------------------------------------------------------------------------------------------------------------------
+-}
+
+-- Implement a function doubleEveryOther :: [Integer] -> [Integer], that doubles every second element of a list of
+-- integers, starting from the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x, y] = [2 * x, y]
+doubleEveryOther (x : y : ints) = 2 * x : y : doubleEveryOther ints
+
+{-
+--------------------------------------------------------------------------------------------------------------------
+Exercise 5
+--------------------------------------------------------------------------------------------------------------------
+-}
+
+-- Implement a function toDigits :: Integer -> [Integer], that takes an integer and returns a list of its digits.
+-- For example, toDigits 123 = [1,2,3]. It should return an empty list if the number is zero or negative
